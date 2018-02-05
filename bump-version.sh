7c0a881c6651d4eb8d4613c0bfdce152eb24414f
stefanovualto <stefano.nepa@vualto.com>
2018-02-05 09:47:54 +0000

    try again 10

--- bump-version.sh
+++ bump-version.sh
@@ -12,9 +12,13 @@ PACKAGE_GIT_VERSION=$(curl -s https://raw.githubusercontent.com/[user]/[repo]/ma
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
+echo $PACKAGE_GIT_VERSION
+echo $PACKAGE_VERSION
+
 if [ "$PACKAGE_GIT_VERSION" == "$PACKAGE_VERSION" ]
 then
-git config --global user.email "vuplay@vualto.com" && git config --global user.name "vuplay" && git config --global push.default simple
-
-npm version patch -m "updated patch version [ci skip]" && git push
+  git config --global user.email "vuplay@vualto.com" && git config --global user.name "vuplay" && git config --global push.default simple
+  echo $PACKAGE_GIT_VERSION
+  echo $PACKAGE_VERSION
+  npm version patch -m "updated patch version [ci skip]" && git push
 fi
