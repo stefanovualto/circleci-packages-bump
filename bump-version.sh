789abe92bb551b5f0f0195dc4d956c1ba6165d34
stefanovualto <stefano.nepa@vualto.com>
2018-02-06 16:21:34 +0000

    try again 12

--- bump-version.sh
+++ bump-version.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-URL=`cat package.json | grep url | head -1`
-URL_WITHOUT_SUFFIX="${URL%.*}"
-REPONAME="$(basename "${URL_WITHOUT_SUFFIX}")"
-GITUSER="$(basename "${URL_WITHOUT_SUFFIX%/${REPONAME}}")"
+# Description: bump version only if the previous master version was the same (not manually modified)
+# Requirements: The local package.json must have an upper property "url" setted with the git url of the project
+git config --global user.email "vuplay@vualto.com" && git config --global user.name "vuplay" && git config --global push.default simple
 
-PACKAGE_GIT_VERSION=$(curl -s https://raw.githubusercontent.com/$GITUSER/$REPONAME/master/package.json \
+git checkout HEAD~1 ../previous
+
+URL=`cat package.json | grep url | head -1`
+
+PREVIOUS_PACKAGE_VERSION=$(cat ../previous/package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
+
+echo "PREVIOUS_PACKAGE_VERSION=${PREVIOUS_PACKAGE_VERSION}" 
 
   PACKAGE_VERSION=$(cat package.json \
   | grep version \
@@ -17,8 +22,10 @@ PACKAGE_GIT_VERSION=$(curl -s https://raw.githubusercontent.com/$GITUSER/$REPONA
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
-if [ "$PACKAGE_GIT_VERSION" == "$PACKAGE_VERSION" ]
+  echo "PACKAGE_VERSION=${PACKAGE_VERSION}" 
+
+if [ "$PREVIOUS_PACKAGE_VERSION" == "$PACKAGE_VERSION" ]
 then
-  git config --global user.email "vuplay@vualto.com" && git config --global user.name "vuplay" && git config --global push.default simple
   npm version patch -m "updated patch version [ci skip]" && git push
+  echo "Version bumped"
 fi
