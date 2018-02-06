9767fa6a85e33907d56cad1431d889a63286f4ad
stefanovualto <stefano.nepa@vualto.com>
2018-02-06 16:25:52 +0000

    try again 13

--- bump-version.sh
+++ bump-version.sh
@@ -4,17 +4,18 @@
 # Requirements: The local package.json must have an upper property "url" setted with the git url of the project
 git config --global user.email "vuplay@vualto.com" && git config --global user.name "vuplay" && git config --global push.default simple
 
-git checkout HEAD~1 ../previous
+git fetch
+git checkout HEAD~1
 
-URL=`cat package.json | grep url | head -1`
-
-PREVIOUS_PACKAGE_VERSION=$(cat ../previous/package.json \
+PREVIOUS_PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
 echo "PREVIOUS_PACKAGE_VERSION=${PREVIOUS_PACKAGE_VERSION}" 
+
+git checkout master
 
   PACKAGE_VERSION=$(cat package.json \
   | grep version \
