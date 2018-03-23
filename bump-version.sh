26b97ba838cb2b861fa83d64f752088cc959cb23
vuplay <vuplay@vualto.com>
2018-03-23 09:47:29 +0000

    v5

--- bump-version.sh
+++ bump-version.sh
@@ -40,18 +40,18 @@ if [ "$PREVIOUS_PACKAGE_VERSION" == "$PACKAGE_VERSION" ]
 then
     npm version patch -m "CircleCi has bumped the patch version to $BUMPED_PACKAGE_VERSION [ci skip]" 
     echo "Version bumped from the old one"
-    TAG_VERSION=BUMPED_PACKAGE_VERSION
+    TAG_VERSION=$BUMPED_PACKAGE_VERSION
 elif [ "$PREVIOUS_PACKAGE_VERSION" == "" ]
 then
     npm version patch -m "CircleCi has bumped the patch version to $BUMPED_PACKAGE_VERSION [ci skip]"
     echo "Version bumped wasn't able to find the previous one"
-    TAG_VERSION=BUMPED_PACKAGE_VERSION
+    TAG_VERSION=$BUMPED_PACKAGE_VERSION
 else
     git commit --allow-empty -m "CircleCi has released a specific version $PACKAGE_VERSION [ci skip]"
     echo "Version wasn't bumped due to a modification of the major or minor version"
-    TAG_VERSION=PACKAGE_VERSION
+    TAG_VERSION=$PACKAGE_VERSION
 fi
 git tag -a v$TAG_VERSION -m "CircleCi has taged this version $TAG_VERSION"
 git push
 
-echo "V4"
+echo "V5"
